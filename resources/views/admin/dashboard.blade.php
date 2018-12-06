8f36716495c801f078ccb53b8d3828b344cbc6cb
vagrant <vagrant@homestead>
2018-12-06 15:17:58 +0000

    Added administration page

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                Всего новостей:
+            </div>
+            <div class="col-sm">
+                Всего изображений в галерее:
+            </div>
+        </div>
+    </div>
+@endsection
+
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
+Route::group(
+    [
+        'prefix' => 'admin',
+        'namespace' => 'Admin',
+        'middleware' => ['auth']
+    ],
+    function () {
+        Route::get('/', 'DashboardController@dashboard')->name('admin.index');
+    }
+);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
