78d3eab6081735ad6b82287c80aa4d60deeddf28
vagrant <vagrant@homestead>
2018-12-11 13:13:28 +0000

    deleted 'Home'  menu item

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item"><a class="nav-link" href="{{route('home')}}">Home</a></li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">@lang('admin.administration')</a>
                             <ul class="dropdown-menu" role="menu">
